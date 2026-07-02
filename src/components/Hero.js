5a1592c4149de720ff3f6ce86cfcff0154640b7c
zErick93op <ericklarreacarlos@outlook.com>
2026-07-01 19:15:29 -0500

    Agregamos Barra Hamburguesa, Nav resposive

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -18,29 +18,29 @@ export default function Hero() {
     <section id="herosection" className="min-h-screen flex flex-col items-center justify-between py-[50px] px-[40px] text-white overflow-x-clip sm:flex-col gap-[15px] lg:flex-row">
 
       {/*Texto la Izquierda*/}
-      <div className="max-w-4xl" data-aos="fade-right" data-aos-duration="3000">
+      <div className="max-w-4xl order-2 sm:order-1 md:order-1 lg:order-1" data-aos="fade-right" data-aos-duration="3000">
         <p className={`${inter.className} text-2xl lg:text-4xl mb-2 font-bold`}>Hola,</p>
         <h1 className={`${inter.className} text-2xl lg:text-4xl mb-2 font-bold`}>Me llamo Erick</h1>
         <h2 className={`${inter.className} text-2xl lg:text-4xl mb-2 font-bold`}>Desarrollador Frontend Jr.</h2>
         <p className={`${inter.className} text-2xl mb-[29px] lg:text-4xl mb-[39px] font-bold`}>Soy desarrollador frontend en formación y en constante aprendizaje y me enfoco en mejorar mis habilidades cada día para crear cosas simples, funcionales y experiencias web atractivas.</p>
 
-      {/*Botones*/}
-      <div className="flex items-center justify-center gap-3">
+        {/*Botones*/}
+        <div className="flex items-center justify-center gap-3">
 
-          <button className={`${inter.className} border border-white/50 px-4 py-2 text-sm rounded-[1rem] font-bold hover:shadow-[#541557] hover:shadow-lg`}>
-            Sobre mí
-          </button>
+            <button className={`${inter.className} border border-white/50 px-4 py-2 text-sm rounded-[1rem] font-bold hover:shadow-[#541557] hover:shadow-lg`}>
+              Sobre mí
+            </button>
 
-          <button className={`${inter.className} border border-white/50 px-4 py-2 text-sm rounded-[1rem] font-bold hover:shadow-[#541557] hover:shadow-lg`}>
-            Contáctame
-          </button>
+            <button className={`${inter.className} border border-white/50 px-4 py-2 text-sm rounded-[1rem] font-bold hover:shadow-[#541557] hover:shadow-lg`}>
+              Contáctame
+            </button>
 
-      </div>
+        </div>
 
       </div>
 
       {/*Imagen a la derecha*/}
-      <div className=" w-[100%] h-[430px] sm:w-[455px] sm:h-[618px] lg:w-[450px] lg:h-[657] rounded-xl" data-aos="fade-left" data-aos-duration="3000">
+      <div className="w-[100%] h-[430px] sm:w-[455px] sm:h-[618px] lg:w-[450px] lg:h-[657] rounded-xl order-1 md:order-2 md:order-2 lg:order-2" data-aos="fade-left" data-aos-duration="3000">
       <img src="/fotoportfolio.png" alt="Foto Portfolio" className="w-[100%] h-[100%]"/>
       </div>
     </section>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,17 @@
+'use client'
 import localFont from "next/font/local";
+import { useState } from "react";
 
 const asimovian = localFont({
   src: "../app/fonts/Asimovian-Regular.ttf",
 });
 
-
 export default function Navbar() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <header className={`${asimovian.className} flex justify-between items-center px-[90px] py-[20px] border-b border-[#E2E8F0]/50 text-white`}>
+    <header className={`${asimovian.className} flex justify-between px-[20px] py-[20px] items-center sm:px-[90px] sm:py-[20px] border-b border-[#E2E8F0]/50 text-white`}>
 
       {/* LOGO */}
       <h1 className="text-3xl" >
@@ -15,11 +19,24 @@ export default function Navbar() {
       </h1>
 
       {/* NAV LINKS */}
-      <nav className="gap-6 text-sm flex gap-6 text-sm">
-        <a href="#aboutme">Sobre mí</a>
-        <a href="#projects">Proyectos</a>
-        <a href="#contact">Contáctame</a>
-      </nav>
+        <nav className="relative">
+          <ul className=" hidden gap-6 text-sm md:flex gap-6 text-sm ">
+            <li><a href="#aboutme">Sobre mí</a></li>
+            <li><a href="#projects">Proyectos</a></li>
+            <li><a href="#contact">Contáctame</a></li>
+          </ul>
+
+          {/*Boton de Barra de Hamburguesa*/}
+          <button className="sm:hidden" onClick={() => setIsOpen(!isOpen)}>{isOpen ? "✕" : "☰"}</button>
+          
+          {isOpen && (           
+            <ul className="flex flex-col gap-4 px-6 py-4 md:hidden absolute top-[2.5rem] right-[-100px] bg-white/10 rounded-sm z-50">
+              <li><a href="#aboutme">Sobre mí</a></li>
+              <li><a href="#projects">Proyectos</a></li>
+              <li><a href="#contact">Contáctame</a></li>
+            </ul>
+          )}
+        </nav>
 
 
       {/*Logo Whastapp*/}
